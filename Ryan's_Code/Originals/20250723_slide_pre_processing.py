d9f4440f0a43b8623067ce2653b592bd4e2cf853
Ryan Corbyn <RCORBYN@BICR.LOCAL>
2025-08-19 10:54:01 +0100

    Working version
    I have worked very hard on getting the image processing working to a level at which the slide relabelling was working well.
    I have managed to get to a point that I have a functional workflow, the steps in the workflow are:
    1. Use an edge detection filter to highlight the letters in the image.
    2. Fill holes to create masks for each of the letters on the slide.
    3. Convert the mask to binary and add in an area filter to remove crap from the slide.
    4. Save these new slide labels that contain just the letters from the slide.
    
    5. In a new script open the new slide labels.
    6. Convert the slide label into an instance segmentation for all letters.
    7. Find the letters in the: Top Left, Top Right, Bottom left and Bottom right.
    8. Calculate the angle of slide rotation using the top let and bottol left letter centroids.
    9 Rotate the image.
    10. Crop the image to just contain the letter masks.
    11. Skeletonize the text to get a better definition of the letters compared to the masks.
    12. Use Tesseract to read the text from the image.
    13. Rename the associated slide with the text from the label.

---
+++ Ryan's_Code/Originals/20250723_slide_pre_processing.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 25 15:26:17 2025
+
+@author: RCORBYN
+"""
+import pytesseract as pytess
+import numpy as np 
+import skimage 
+import scipy 
+import aicsimageio
+import os 
+import tifffile as tf
+from PIL import Image
+import tkinter as tk 
+import pandas as pd
+import matplotlib.pyplot as plt
+from tkinter import filedialog
+import sys
+
+image_param = sys.argv[1]
+
+rgba_image = Image.open(image_param)
+im_data = rgba_image.convert('RGB')
+# Convert to a numpy array
+im_data = np.array(im_data)
+
+#######
+# Perform edge detection
+edges = skimage.filters.sobel( np.sum(im_data, axis = 2) )
+
+#####
+# Threshold the image to remove most of the 
+# unimportant information from the image.
+
+# Find the threshold values
+threshold_val = skimage.filters.threshold_otsu(edges)
+edges[edges < threshold_val] = 0
+
+# Fill the binary masks of the letters
+fill_string = scipy.ndimage.binary_fill_holes(edges)
+
+# Invert the image and multiply by the letter
+# mask to get only the letters in the image. 
+invert = fill_string * skimage.util.invert( im_data[:, :, 0] )
+#####
+# Create a binary image from the inverted letters
+binary = np.array(invert)
+binary[binary>0] = 1
+
+# create labels from the binary image. 
+labels = skimage.measure.label(binary)
+
+# Get properties of the letters and remaining masks. 
+region_props = pd.DataFrame( skimage.measure.regionprops_table(
+                labels, properties = {'label', 'area', 
+                'axis_minor_length', 'axis_major_length'}) )
+
+# Create a mask of zeros
+filtered_image = np.zeros(im_data.shape)
+
+# Loop around all labels. 
+for i in range(region_props.shape[0]): 
+    # Find the positon of mask i in the label image. 
+    mask_pos = np.where(labels == region_props['label'].iloc[i])
+    # Find the maximum and minimum height and width of the masks. 
+    delta_x = np.abs( np.min(mask_pos[0]) - np.max(mask_pos[0]) ) 
+    delta_y = np.abs( np.min(mask_pos[1]) - np.max(mask_pos[1]) ) 
+    # If the mask is too long or high, remove 
+    # Also remove if the area is too small. 
+    if delta_x  <= 75 and delta_y <= 75 and region_props['area'].iloc[i] > 100: 
+        # keep the mask. 
+        filtered_image[mask_pos[0], mask_pos[1]] = skimage.util.invert(
+                                    im_data[mask_pos[0], mask_pos[1], :] )
+
+filtered_image = np.array(filtered_image, dtype = 'uint8')
+
+filtered_image[filtered_image>150] = 255
+filtered_image[filtered_image<150] = 0
+
+im = Image.fromarray(filtered_image)
+im.save("out_pre.png")
